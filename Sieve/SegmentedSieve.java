b37ce7f6b40a9fc6e21542a56f0a57a0ad359cad
Tathagata Guha Ray <nguharay71@gmail.com>
2023-08-19 17:05:58 +0530

    added comments for explanation

--- Sieve/SegmentedSieve.java
+++ Sieve/SegmentedSieve.java
@@ -1,46 +1,46 @@
 
 import java.util.*;
 
-public class SegmentedSieve{
-      
-public static void simpleSieve(int num,Vector<Integer> prime){
+public class SegmentedSieve {
 
-		boolean bool[] = new boolean[num+1];
-		int n;
+    public static void simpleSieve(int num, Vector<Integer> prime) {
 
-		n=(num==2)? 2:(int)Math.sqrt(num);
-	        
-		for (int p=2; p<=n; p++)
-			if (bool[p] == false)
-				for (int i=p*p; i<=num; i+=p)
-					bool[i] = true;
-	
-		for (int p=2; p<=num; p++)
-			if (bool[p] == false)
-				prime.add(p);
-}
+        boolean bool[] = new boolean[num + 1];                  // Array of `n+1` boolean values
+        int n;
 
+        n = (num == 2) ? 2 : (int) Math.sqrt(num);
 
-public static void main(String args[]) {
-    Vector<Integer> prime = new Vector<>();
-	Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the range");
-      int low = sc.nextInt(),high = sc.nextInt();
-      System.out.println("");
-      simpleSieve((int)Math.sqrt(high),prime);
-  
-      int i,j,Ans[]= new int[high-low];
-  
-    for(i=0;i<Ans.length;i++)
-          Ans[i]=low+i;
+        for (int p = 2; p <= n; p++)                            // Looping through 2 to square root of `n`  
+            if (bool[p] == false)
+                for (int i = p * p; i <= num; i += p)           // Looping through multiples of `i`, eliminating all composites
+                    bool[i] = true;
 
-    for(i=0;i<prime.size();i++)
-        for(j=0;j<Ans.length;j++)
-            if(Ans[j]%prime.get(i)==0 && Ans[j] != prime.get(i))
-                Ans[j]=0;
+        for (int p = 2; p <= num; p++)                          // Looping through bool array
+            if (bool[p] == false)
+                prime.add(p);
+    }
     
-    for(j=0;j<Ans.length;j++)
-        if(Ans[j]!=0 && Ans[j] !=1)
-        System.out.print(Ans[j]+" ");
+
+    public static void main(String args[]) {
+        Vector<Integer> prime = new Vector<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range");
+        int low = sc.nextInt(), high = sc.nextInt();
+        System.out.println("");
+        simpleSieve((int) Math.sqrt(high), prime);              // Calling simpleSieve() to generate all primes smaller than or equal to square root of `high`
+
+        int i, j, Ans[] = new int[high - low];                  // Array of size `high-low`
+
+        for (i = 0; i < Ans.length; i++)                        // Initializing array with values from `low` to `high`
+            Ans[i] = low + i;
+
+        for (i = 0; i < prime.size(); i++)                      // Looping through all primes smaller than or equal to square root of `high`
+            for (j = 0; j < Ans.length; j++)
+                if (Ans[j] % prime.get(i) == 0 && Ans[j] != prime.get(i))               // Eliminating all composites
+                    Ans[j] = 0;
+
+        for (j = 0; j < Ans.length; j++)
+            if (Ans[j] != 0 && Ans[j] != 1)
+                System.out.print(Ans[j] + " ");
     }
 }
--- Sieve/SimpleSieve.java
+++ Sieve/SimpleSieve.java
@@ -1,24 +1,37 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class SimpleSieve {
-   public static void main(String args[]) {
+	public static void main(String args[]) {
 
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a number");
-      int num = sc.nextInt();
+		Vector<Integer> prime = new Vector<>();
 
-      boolean[] bool = new boolean[num+1];
-     
-      for (int i = 2; i<= Math.sqrt(num); i++) 
-         if(bool[i] == false)
-            for(int j = 2*i; j<=num; j+=i) 
-               bool[j] = true;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");			// Enter Number `n`
+		int num = sc.nextInt();
 
-      System.out.println("List of prime numbers upto given number are : ");
-      for (int i = 2; i< bool.length; i++)
-         if(bool[i]==false)
-            prime.add(i);
-   }
+		boolean[] bool = new boolean[num + 1];			// Array of `n+1` boolean values
+
+		for (int i = 2; i <= Math.sqrt(num); i++)		// Looping through 2 to square root of `n`
+			if (bool[i] == false)
+				for (int j = 2 * i; j <= num; j += i)	// Looping through multiples of `i`, eliminating all composites
+					bool[j] = true;
+
+		/* Say n = 9.
+		 *
+		 * So, bool = [false, false, false, false, false, false, false, false, false]
+		 * 
+		 * In for loop, condition: i <= 3
+		 * Iteration 1: bool[4] = bool[6] = bool[8] = true
+		 * Iteration 2: bool[9] = true
+		 */
+
+		System.out.println("List of prime numbers upto given number are : ");
+		for (int i = 2; i < bool.length; i++)			// Looping through bool array
+			if (bool[i] == false) 
+				prime.add(i);
+
+		System.out.println(prime);
+
+	}
 }
-
-20BCD7109
